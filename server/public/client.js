5674fc326d7346f72793e451d515dd0900d4975f
Adam Oberle <oberle.adam@gmail.com>
2023-05-11 21:41:59 -0500

    created jquery and function for clear button in client.js to clear input fields on click, tested and working

--- server/public/client.js
+++ server/public/client.js
@@ -2,4 +2,9 @@ $(document).ready(onReady);
 
 function onReady(){
     console.log( 'jq')
+    $('#clear-button').on('click', clearInputs)
+}
+
+function clearInputs () {
+    $('.inputTextBox').val('')
 }
